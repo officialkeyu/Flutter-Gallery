4898a6cc22bcd6144c87f2a464d9e703991f6e87
keyurzala <keyur.zala.sa@gmail.com>
2024-09-12 12:28:48 +0530

    - flutter web galley page

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:tech_test/ui/myapp.dart';
 
 /// The main entry point of the application.
 Future<void> main() async {
-  // Loads environment variables from the .env file.
+  /// Loads environment variables from the .env file.
   await dotenv.load();
-  // Runs the Flutter application with a [ProviderScope] to manage state using Riverpod.
+  /// Runs the Flutter application with a [ProviderScope] to manage state using Riverpod.
   runApp(const ProviderScope(child: MyApp()));
 }
